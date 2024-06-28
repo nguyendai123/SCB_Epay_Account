3c5bbf582b63513ae8d71ecc29d61eda440ec8e7
tungnh <tungnh@protontech.net.vn>
2024-06-28 11:01:54 +0700

    init account-service

---
+++ src/main/java/com/ewallet/AccountServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+
+}
